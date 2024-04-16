9ae5d7a9febfc39e3522f38da07221f119c79dfb
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-04-16 12:05:30 +0900

    As of 202404161205

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -11,7 +11,7 @@ fn first_word(s: &String) -> &str {
 }
 
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
     let word = first_word(&s); // word will get the value 5
 
