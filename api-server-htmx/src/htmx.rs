100373cb83c72e4f714252ac90c5d39d3084e00c
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-07-03 18:13:05 +0900

    As of 202407031813

--- api-server-htmx/src/htmx.rs
+++ api-server-htmx/src/htmx.rs
@@ -11,6 +11,32 @@ use tracing::error;
 
 use crate::models::{Customer, Params};
 
+/// Creates the API router with the given SQLite pool.
+pub fn create_router(pool: SqlitePool) -> ApiRouter {
+    ApiRouter::new()
+        .api_route("/content.top", get(content_top))
+        .api_route("/content.list", get(content_list))
+        .api_route("/content.list.tbody", get(content_list_tbody))
+        .with_state(pool)
+}
+
+/// Represents a template for rendering the content list.
+#[derive(Template)]
+#[template(path = "content.top.j2")]
+struct ContentTopTemplate {
+    title: String,
+}
+
+/// Handles the content list request.
+async fn content_top(headers: HeaderMap) -> Result<Html<String>, Response> {
+    check_hx_request(&headers)?;
+
+    let template = ContentTopTemplate {
+        title: "Htmx Spa Top".to_string(),
+    };
+    Ok(Html(template.render().unwrap()))
+}
+
 /// Represents a template for rendering the content list.
 #[derive(Template)]
 #[template(path = "content.list.j2")]
@@ -18,40 +44,6 @@ struct ContentListTemplate {
     title: String,
     skip_next: i32,
     limit: i32,
-}
-
-/// Represents a template for rendering the content list table body.
-#[derive(Template)]
-#[template(path = "content.list.tbody.j2")]
-struct ContentListTbodyTemplate {
-    skip_next: i32,
-    limit: i32,
-    customers: Vec<Customer>,
-}
-
-/// Represents an error response.
-#[derive(Serialize)]
-struct ErrorResponse {
-    detail: String,
-}
-
-impl IntoResponse for ErrorResponse {
-    fn into_response(self) -> Response {
-        (StatusCode::BAD_REQUEST, axum::Json(self)).into_response()
-    }
-}
-
-/// Checks if the request is an HX request.
-/// Returns an error response if the request is not an HX request.
-fn check_hx_request(headers: &HeaderMap) -> Result<(), Response> {
-    if headers.get("HX-Request").is_none() {
-        Err(ErrorResponse {
-            detail: "Only HX request is allowed to this endpoint.".to_string(),
-        }
-        .into_response())
-    } else {
-        Ok(())
-    }
 }
 
 /// Handles the content list request.
@@ -64,6 +56,15 @@ async fn content_list(headers: HeaderMap) -> Result<Html<String>, Response> {
         limit: 2,
     };
     Ok(Html(template.render().unwrap()))
+}
+
+/// Represents a template for rendering the content list table body.
+#[derive(Template)]
+#[template(path = "content.list.tbody.j2")]
+struct ContentListTbodyTemplate {
+    skip_next: i32,
+    limit: i32,
+    customers: Vec<Customer>,
 }
 
 /// Handles the content list table body request.
@@ -96,10 +97,27 @@ async fn content_list_tbody(
     Ok(Html(template.render().unwrap()))
 }
 
-/// Creates the API router with the given SQLite pool.
-pub fn create_router(pool: SqlitePool) -> ApiRouter {
-    ApiRouter::new()
-        .api_route("/content.list", get(content_list))
-        .api_route("/content.list.tbody", get(content_list_tbody))
-        .with_state(pool)
+/// Represents an error response.
+#[derive(Serialize)]
+struct ErrorResponse {
+    detail: String,
+}
+
+impl IntoResponse for ErrorResponse {
+    fn into_response(self) -> Response {
+        (StatusCode::BAD_REQUEST, axum::Json(self)).into_response()
+    }
+}
+
+/// Checks if the request is an HX request.
+/// Returns an error response if the request is not an HX request.
+fn check_hx_request(headers: &HeaderMap) -> Result<(), Response> {
+    if headers.get("HX-Request").is_none() {
+        Err(ErrorResponse {
+            detail: "Only HX request is allowed to this endpoint.".to_string(),
+        }
+        .into_response())
+    } else {
+        Ok(())
+    }
 }
